473773707c6ad1cd2bdbae3e96fcb91b0030933f
XavierFarrus <xavier.farrus@gracia.lasalle.cat>
2025-12-10 17:01:57 +0100

    Extra

--- main.ts
+++ main.ts
@@ -3,12 +3,12 @@ input.onGesture(Gesture.Shake, function () {
     tool = randint(0, 2)
     if (tool == 0) {
         basic.showIcon(IconNames.SmallSquare)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
     } else if (tool == 1) {
         basic.showIcon(IconNames.Square)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     } else {
         basic.showIcon(IconNames.Scissors)
+        music.play(music.createSoundExpression(WaveShape.Square, 1615, 1, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
     }
-})
-basic.forever(function () {
-	
 })
